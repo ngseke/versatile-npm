a1b8adc34702522e361a34438f0a34cd7aed5a64
ngseke <ngseke@gmail.com>
2023-11-21 21:09:09 +0800

    chore: init ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue']
+  },
+  ignorePatterns: ['dist'],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
