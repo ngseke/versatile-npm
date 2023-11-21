f6b737d32bf4e1eff91e0b1422c92659870c6f28
ngseke <ngseke@gmail.com>
2023-11-21 21:40:26 +0800

    feat: setup @crxjs/vite-plugin

---
+++ manifest.config.ts
@@ -0,0 +1,36 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import packageJson from './package.json'
+
+const { version } = packageJson
+
+export default defineManifest(async () => ({
+  manifest_version: 3,
+  version,
+
+  default_locale: 'en',
+  name: '__MSG_appName__',
+  description: '__MSG_appDesc__',
+
+  icons: {
+    128: 'src/assets/img/icon.png',
+  },
+  action: {
+    default_popup: 'src/pages/popup/index.html',
+    default_icon: 'src/assets/img/icon.png',
+  },
+  content_scripts: [
+    {
+      matches: [
+        'https://www.npmjs.com/*',
+        'https://classic.yarnpkg.com/*',
+      ],
+      js: ['src/pages/content/index.ts'],
+      run_at: 'document_start',
+    },
+  ],
+  options_ui: {
+    open_in_tab: true,
+    page: 'src/pages/options/index.html',
+  },
+  permissions: ['storage'],
+}))
---
+++ src/pages/options/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Options</div>
+</template>
+
+<style scoped>
+</style>
