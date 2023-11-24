48eebbcba9a733c93122096c2c962ded0a5f7606
ngseke <ngseke@gmail.com>
2023-11-24 16:25:25 +0800

    test: create `vitest-setup.ts`

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    setupFiles: [
+      './__tests__/setup.ts',
+    ],
+    globals: true,
+    environment: 'jsdom',
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
+
+  },
+})
