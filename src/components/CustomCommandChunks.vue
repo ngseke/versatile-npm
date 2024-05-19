1bc8d9aab05f57d6771fbf9aa4c77f5ebe54a23b
ngseke <ngseke@gmail.com>
2024-05-19 12:04:41 +0800

    feat: display version template with style

--- src/components/CustomCommandChunks.vue
+++ src/components/CustomCommandChunks.vue
@@ -12,19 +12,20 @@ const chunks = computed(() => parseCustomCommand(props.modelValue))
 </script>
 
 <template>
-  <template
-    v-for="chunk in chunks"
-    :key="chunk.id"
-  >
-    <span v-if="chunk.type === 'text'">{{ chunk.value }}</span>
+  <template v-for="chunk in chunks" :key="chunk.id">
     <span
-      v-else-if="chunk.type === 'manager'"
+      v-if="chunk.type === 'manager'"
       class="font-weight-bold"
     >{{ chunk.value }}</span>
     <span
       v-else-if="chunk.type === 'name'"
-      class="font-weight-bold gradient"
+      class="font-weight-bold gradient-name"
     >{{ chunk.value }}</span>
+    <span
+      v-else-if="chunk.type === 'version'"
+      class="font-weight-bold gradient-version"
+    >{{ chunk.value }}</span>
+    <span v-else>{{ chunk.value }}</span>
   </template>
 </template>
 
@@ -32,8 +33,13 @@ const chunks = computed(() => parseCustomCommand(props.modelValue))
 span
   white-space: pre
 
-.gradient
+.gradient-name
   -webkit-background-clip: text
   -webkit-text-fill-color: transparent
   background-image: linear-gradient(150deg, #F44336, #fe5196)
+
+.gradient-version
+  -webkit-background-clip: text
+  -webkit-text-fill-color: transparent
+  background-image: linear-gradient(150deg, #4facfe, #00f2fe)
 </style>
