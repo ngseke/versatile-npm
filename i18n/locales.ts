8810b8f8de7606dabaa0763b5943d8d694142650
ngseke <ngseke@gmail.com>
2025-01-01 17:55:57 +0800

    feat: add languages

--- i18n/locales.ts
+++ i18n/locales.ts
@@ -1,4 +1,11 @@
 export const locales = [
   { code: 'en', name: 'English' },
+  { code: 'es', name: 'Español' },
+  { code: 'fr', name: 'Français' },
+  { code: 'it', name: 'Italiano' },
+  { code: 'pt', name: 'Português' },
+  { code: 'vi', name: 'Tiếng Việt' },
+  { code: 'ja', name: '日本語' },
+  { code: 'ko', name: '한국어' },
   { code: 'zh-Hant', name: '中文' },
 ] as const
